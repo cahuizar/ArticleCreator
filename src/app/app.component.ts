36f2ef7b8250c21aa13bd8c38d2ca098a2d5403d
cahuizar <Carlos.Huizar@navient.com>
2016-12-19 16:08:11 -0500

    basic article creator completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,20 @@ export class AppComponent {
 
   constructor() {
     this.articles = [
-      new Article('Angular 2', 'htttp://angular.io', 3),
-      new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1)
+      new Article('angular site', 'http://angular.io', 10),
+      new Article('Google', 'http://google.com', 8),
+      new Article('Carlos', 'http://carloshuizar.com', 50)
     ];
+  }
+  sortedArticles() : Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 
   addArticle(title : HTMLInputElement, link : HTMLInputElement) : boolean {
       console.log(`Adding article title: ${title.value} and link ${link.value}`);
       this.articles.push(new Article(title.value, link.value, 0));
+      title.value = '';
+      link.value = '';
       return false;
   }
 }
